1b4e135b95d007ff8f768fb9eaf68c17eb28e4ea
Arjun Manoj <arjunarsh1330@gmail.com>
2026-09-05 14:33:17 +0530

    Time: 643 ms (47.32%), Space: 0B (100%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,12 @@
+select e.employee_id
+from Employees e
+left join Salaries s
+on e.employee_id=s.employee_id
+where salary is null
+UNION
+select s.employee_id
+from Employees e
+right join Salaries s
+on e.employee_id=s.employee_id
+where name is null
+order by employee_id asc;
